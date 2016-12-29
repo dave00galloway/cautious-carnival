b06e0d796262f46cc743476b32bb7017cba3208d
dave00galloway <david_a_galloway@yahoo.co.uk>
2016-12-29 22:05:54 +0000

    faff around with io

--- src/test/java/iloveyouboss/test/scratch/AssertTest.java
+++ src/test/java/iloveyouboss/test/scratch/AssertTest.java
@@ -3,9 +3,15 @@ package iloveyouboss.test.scratch;
 import iloveyouboss.Account;
 import iloveyouboss.InsufficientFundsException;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -57,5 +63,16 @@ public class AssertTest {
         thrown.expect(InsufficientFundsException.class);
         thrown.expectMessage("amount:- 100");
         account.withdraw(100);
+    }
+
+    @Test
+    @Ignore
+    public void readsFromTestFile() throws Throwable {
+        String filename = "test.txt";
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        writer.write("test data");
+        writer.close();
+        // ...
+        Files.deleteIfExists(new File(filename).toPath());
     }
 }
